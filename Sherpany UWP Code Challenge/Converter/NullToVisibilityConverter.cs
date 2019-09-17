ce581b723f42a5f9c75f510a2105b878f094555b
Tristan <tristan.robin69@gmail.com>
2019-09-17 09:00:33 +0200

    Values list retrieval Implemented the sherpany values page to display the values list and selected value details. UI is pretty simple.
    Signed-off-by: Tristan <tristan.robin69@gmail.com>

---
+++ Sherpany UWP Code Challenge/Converter/NullToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Sherpany_UWP_Code_Challenge.Converter
+{
+    public class NullToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value == null ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Sherpany UWP Code Challenge/ViewModel/Pages/SherpanyValuesPageViewModel.cs
+++ Sherpany UWP Code Challenge/ViewModel/Pages/SherpanyValuesPageViewModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 using Sherpany_UWP_Code_Challenge.Interfaces;
+using Sherpany_UWP_Code_Challenge.Model;
 
 namespace Sherpany_UWP_Code_Challenge.ViewModel.Pages
 {
@@ -12,9 +16,60 @@ namespace Sherpany_UWP_Code_Challenge.ViewModel.Pages
     {
         private readonly IDummyApiService _apiService;
 
+        public ObservableCollection<SherpanyValueModel> Values { get; } = new ObservableCollection<SherpanyValueModel>();
+        private SherpanyValueModel _detailedValue;
+
+        public SherpanyValueModel DetailedValue
+        {
+            get { return _detailedValue; }
+            set
+            {
+                if (_detailedValue != value)
+                {
+                    _detailedValue = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+
+        public ICommand GetValuesCommand { get; }
+        private bool _valuesRetrievalInProgress = false;
+
+        public bool ValuesRetrievalInProgress
+        {
+            get
+            {
+                return _valuesRetrievalInProgress;
+            }
+            set
+            {
+                if(value != _valuesRetrievalInProgress)
+                {
+                    _valuesRetrievalInProgress = value;
+                    ((RelayCommand)GetValuesCommand).RaiseCanExecuteChanged();
+                }
+            }
+        }
+
+
         public SherpanyValuesPageViewModel(IDummyApiService apiService)
         {
             _apiService = apiService;
+            GetValuesCommand = new RelayCommand(GetValues, () => !ValuesRetrievalInProgress);
+        }
+
+        private async void GetValues()
+        {
+            ValuesRetrievalInProgress = true;
+
+            Values.Clear();
+            foreach(var value in (await _apiService.GetValueModelsAsync()).OrderBy(e => e.Order))
+            {
+                Values.Add(value);
+            }
+
+            ValuesRetrievalInProgress = false;
         }
     }
 }
